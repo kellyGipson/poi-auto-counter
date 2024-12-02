01b4d662793859d5b78226b305d8a318e63ba62c
Kelly Gipson <thomaskellygipson@gmail.com>
2024-12-02 02:04:46 -0500

    added decrement; added button styling; added count animation when buttons interacted with

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -4,26 +4,51 @@ import { MatButtonModule } from "@angular/material/button";
 @Component({
 	selector: 'counter',
 	template: `
-		<div>
-			<span>{{count}}</span>
-			<button mat-flat-button color="secondary" (click)="increment()">+</button>
-			<button mat-flat-button color="secondary" (click)="decrement()">-</button>
+		<div class="h-full w-full flex justify-center items-center">
+			<div
+				class="w-min gap-8 flex flex-col"
+				[class.translate-y-[-6px]]="isIncrementing"
+			>
+				<div class="w-full flex justify-center">
+					<div
+						[class.text-6xl]="!isIncrementing"
+						[class.text-7xl]="isIncrementing"
+						[class.scale-100]="isIncrementing"
+						[class.translate-y-[2px]]="isIncrementing"
+					>{{count}}</div>
+				</div>
+
+				<div class="counter-buttons">
+					<button class="btn dec" (click)="decrement()">-</button>
+
+					<button class="btn inc" (click)="increment()">+</button>
+				</div>
+			</div>
 		</div>
 	`,
-	imports: [MatButtonModule]
+	imports: [MatButtonModule],
+	styleUrl: './counter.component.scss',
 })
 export class CounterComponent {
 	count = 0;
 	step = 1;
+	isIncrementing = false;
+
+	@HostListener('window:keyup.0')
+	decrement() {
+		this.count -= this.step;
+	}
 
 	@HostListener('window:keyup.enter')
 	@HostListener('window:keyup.space')
 	increment() {
 		this.count += this.step;
+		this.isIncrementing = false;
 	}
 
-	@HostListener('window:keyup.0')
-	decrement() {
-		this.count -= this.step;
+	@HostListener('window:keydown.enter')
+	@HostListener('window:keydown.space')
+	preIncrement() {
+		this.isIncrementing = true;
 	}
 }
