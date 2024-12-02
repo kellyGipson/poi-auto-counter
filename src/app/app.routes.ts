b30fa70459d32d10816c6eabcc306f02d1752f0c
Kelly Gipson <thomaskellygipson@gmail.com>
2024-12-01 22:51:07 -0500

    added temporary routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '**',
+		redirectTo: 'counter'
+	},
+	{
+		path: 'counter',
+		component: CounterComponent,
+	},
+];
