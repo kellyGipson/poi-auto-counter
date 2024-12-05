dac9386dcaf8c9ef9b4c6d3f383271b2bfcaa899
Kelly Gipson <thomaskellygipson@gmail.com>
2024-12-04 22:42:07 -0500

    added home component, route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,18 @@
 import { Routes } from '@angular/router';
 import { CounterComponent } from './counter/counter.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+	{
+		path: '',
+		component: HomeComponent,
+	},
 	{
 		path: 'counter',
 		component: CounterComponent,
 	},
 	{
 		path: '**',
-		redirectTo: 'counter'
+		redirectTo: ''
 	},
 ];
