3cdcbbc5324b33412ea89ba2463f87d9fa9f37d7
Kelly Gipson <thomaskellygipson@gmail.com>
2024-12-05 03:31:31 -0500

    fixed counter page being off center; moved image below selector; improved screen selector design minimally

--- src/app/auto-counter/screen-selector/screen-selector.component.ts
+++ src/app/auto-counter/screen-selector/screen-selector.component.ts
@@ -4,23 +4,28 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Display } from '../../screenshot-desktop-types';
 import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
 	selector: 'screen-selector',
 	template: `
+		<div class="w-min flex flex-col gap-4">
+			<mat-select [formControl]="displayFormControl">
+				@for (display of displayList; track display.id; let idx = $index) {
+					<mat-option [value]="display.id">Display {{ idx + 1 }} ({{ display.width }}x{{ display.height }})</mat-option>
+				}
+			</mat-select>
+
+			<button mat-raised-button (click)="onClick()">get screenshot</button>
+		</div>
+
 		<img *ngIf="screenshot" [src]="screenshot">
-		
-		<mat-select [formControl]="displayFormControl">
-			@for (display of displayList; track display.id; let idx = $index) {
-				<mat-option [value]="display.id">Display {{ idx + 1 }} ({{ display.width }}x{{ display.height }})</mat-option>
-			}
-		</mat-select>
-		<button (click)="onClick()">get screenshot</button>
 	`,
 	imports: [
 		CommonModule,
 		ReactiveFormsModule,
 		MatSelectModule,
+		MatButtonModule,
 	],
 })
 export class ScreenSelectorComponent {
