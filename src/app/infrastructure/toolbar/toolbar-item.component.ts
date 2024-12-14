29c3be0c96365dd905c81b53669d54f092a7eab2
Kelly Gipson <thomaskellygipson@gmail.com>
2024-12-14 10:39:44 -0500

    decreased nav menu on hover; cleaned up hovering state for nav menu items

--- src/app/infrastructure/toolbar/toolbar-item.component.ts
+++ src/app/infrastructure/toolbar/toolbar-item.component.ts
@@ -3,13 +3,17 @@ import { Component, Input } from '@angular/core';
 @Component({
 	selector: 'toolbar-item',
 	template: `
-		<div class="h-12 flex items-center pl-2 gap-3">
-			<div class="bg-slate-400 h-8 w-1 transition-all" [class.w-3]="isHovering"></div>
+		<div
+			class="h-12 flex items-center pl-2 gap-3"
+			(mouseenter)="isHovering = true"
+			(mouseleave)="isHovering = false"
+		>
+			<div class="bg-slate-400 h-8 w-1 transition-all" [class.w-2]="isHovering"></div>
 			
 			<ng-content></ng-content>
 		</div>
 	`
 })
 export class ToolbarItemComponent {
-	@Input() isHovering = false;
+	isHovering = false;
 }
--- src/app/infrastructure/toolbar/toolbar.component.ts
+++ src/app/infrastructure/toolbar/toolbar.component.ts
@@ -1,33 +1,14 @@
 import { Component } from '@angular/core';
 import { ToolbarItemComponent } from './toolbar-item.component';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
 	selector: 'toolbar',
 	template: `
-		<toolbar-item
-			[isHovering]="isHomeHovering"
-			(mouseenter)="isHomeHovering = true"
-			(mouseleave)="isHomeHovering = false"
-			routerLink="/home"
-			routerLinkActive="active"
-		>Home</toolbar-item>
-
-		<toolbar-item
-			[isHovering]="isCounterHovering"
-			(mouseenter)="isCounterHovering = true"
-			(mouseleave)="isCounterHovering = false"
-			routerLink="/counter"
-			routerLinkActive="active"
-		>Counter</toolbar-item>
-
-		<toolbar-item
-			[isHovering]="isAutoCounterHovering"
-			(mouseenter)="isAutoCounterHovering = true"
-			(mouseleave)="isAutoCounterHovering = false"
-			routerLink="/auto-counter"
-			routerLinkActive="active"
-		>Auto Counter</toolbar-item>
+		<toolbar-item routerLink="/home" routerLinkActive="active">Home</toolbar-item>
+		<toolbar-item routerLink="/hunts" routerLinkActive="active">Hunts</toolbar-item>
+		<toolbar-item routerLink="/counter" routerLinkActive="active">Counter</toolbar-item>
+		<toolbar-item routerLink="/auto-counter" routerLinkActive="active">Auto Counter</toolbar-item>
 	`,
 	imports: [
 		ToolbarItemComponent,
@@ -35,8 +16,4 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 		RouterLinkActive,
 	]
 })
-export class ToolbarComponent {
-	isHomeHovering = false;
-	isCounterHovering = false;
-	isAutoCounterHovering = false;
-}
+export class ToolbarComponent {}
